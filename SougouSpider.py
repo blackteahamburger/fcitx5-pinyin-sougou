fc7ca09e86c3db4c7df8d00dfde6c0f6806009bf
blackteahamburger <blackteahamburger@outlook.com>
2024-10-02 01:50:37 +0000

    SougouSpider.py: add multi-thread download

--- SougouSpider.py
+++ SougouSpider.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-import os
+from pathlib import Path
 import argparse
+import concurrent.futures
 
 
 class SougouSpider:
@@ -22,109 +23,123 @@ class SougouSpider:
 		except requests.exceptions.Timeout as e:
 			print(e)
 
-	def download(self, download_urls, category_path):
-		cnt = 0
-		for key_download, url_download in download_urls.items():
-			file_path = category_path + "/" + key_download + ".scel"
-			if os.path.exists(file_path):
-				print(f"{key_download}.scel already exists, skipping...")
-			else:
-				with open(file_path, "wb") as fw:
-					fw.write(self.get_html(url_download).content)
-					print(f"{key_download}.scel download succeeded.")
-					cnt += 1
-		return cnt
+	def download(self, name, url, category_path):
+		file_path = category_path / (name + ".scel")
+		if file_path.is_file():
+			print(f"{file_path} already exists, skipping...")
+		else:
+			file_path.write_bytes(self.get_html(url).content)
+			print(f"{file_path.name} download succeeded.")
+
+	def get_download_links(self, category_url, page, category_path):
+		download_urls = {}
+		for dict_td_list in BeautifulSoup(
+			self.get_html(category_url + "/default/" + str(page)).text,
+			"html.parser",
+		).find_all("div", class_="dict_detail_block"):
+			key_download = dict_td_list.find("div", class_="detail_title").a
+			download_urls[
+				# For dictionaries like 天线行业/BSA
+				key_download.string.replace("/", "-")
+				.replace(",", "-")
+				.replace("|", "-")
+				.replace("\\", "-")
+				.replace("'", "-")
+				if key_download.string
+				# For dictionaries without a name like index 15946
+				else BeautifulSoup(
+					self.get_html(
+						"https://pinyin.sogou.com" + key_download["href"]
+					).text,
+					"html.parser",
+				)
+				.find("div", class_="dict_info_str")
+				.string
+			] = dict_td_list.find("div", class_="dict_dl_btn").a["href"]
+		return download_urls, category_path
 
 	def download_dicts(self, save_path, categories=None, skip_category=False):
-		if not os.path.exists(save_path):
-			try:
-				os.mkdir(save_path)
-			except Exception as e:
-				print(e)
-		if not categories:
+		save_path.mkdir(parents=True, exist_ok=True)
+		if categories is None:
 			categories = ["0"]
 			for dict_nav_list in BeautifulSoup(
 				self.get_html("https://pinyin.sogou.com/dict/cate/index/1").text,
 				"html.parser",
 			).find_all("li", class_="nav_list"):
 				categories.append(dict_nav_list.a["href"].split("/")[-1])
-		cnt = 0
-		for category in categories:
-			category_path = save_path + "/" + category
-			if not os.path.exists(category_path):
-				try:
-					os.mkdir(category_path)
-				except Exception as e:
-					print(e)
-			elif skip_category:
-				print(f"Category {category} already exists, skipping...")
-				continue
-			if category == "0":  # For dictionaries that do not belong to any categories
-				download_urls = {
-					"网络流行新词【官方推荐】": "https://pinyin.sogou.com/d/dict/download_cell.php?id=4&name=网络流行新词【官方推荐】"
-				}
-				for dict_td_list in BeautifulSoup(
-					self.get_html("https://pinyin.sogou.com/dict/detail/index/4").text,
-					"html.parser",
-				).find_all("div", class_="rcmd_dict"):
-					download_urls[
-						dict_td_list.find("div", class_="rcmd_dict_title").a.string
-					] = (
-						"https:"
-						+ dict_td_list.find("div", class_="rcmd_dict_dl_btn").a["href"]
+		with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
+			result_futures = []
+			for category in categories:
+				category_path = save_path / category
+				if category_path.is_dir() and skip_category:
+					print(f"Category {category} already exists, skipping...")
+					continue
+				category_path.mkdir(exist_ok=True)
+				# For dictionaries that do not belong to any categories
+				if category == "0":
+					executor.submit(
+						self.download,
+						"网络流行新词【官方推荐】",
+						"https://pinyin.sogou.com/d/dict/download_cell.php?id=4&name=网络流行新词【官方推荐】",
+						category_path,
 					)
-				cnt += self.download(download_urls, category_path)
-			else:
-				if category == "167":  # Category 167 does not have a real page
-					category_urls = []
 					for dict_td_list in BeautifulSoup(
 						self.get_html(
-							"https://pinyin.sogou.com/dict/cate/index/180"
+							"https://pinyin.sogou.com/dict/detail/index/4"
 						).text,
 						"html.parser",
-					).find_all("div", class_="citylistcate"):
-						category_urls.append(
-							"https://pinyin.sogou.com" + dict_td_list.a["href"]
+					).find_all("div", class_="rcmd_dict"):
+						executor.submit(
+							self.download,
+							dict_td_list.find("div", class_="rcmd_dict_title").a.string,
+							"https:"
+							+ dict_td_list.find("div", class_="rcmd_dict_dl_btn").a[
+								"href"
+							],
+							category_path,
 						)
 				else:
-					category_urls = [
-						"https://pinyin.sogou.com/dict/cate/index/" + category
-					]
-				for category_url in category_urls:
-					pages = (
-						BeautifulSoup(self.get_html(category_url).text, "html.parser")
-						.find("div", id="dict_page_list")
-						.find_all("a")
-					)
-					page_n = 2 if len(pages) < 2 else int(pages[-2].string) + 1
-					for page in range(1, page_n):
-						download_urls = {}
-						for dict in BeautifulSoup(
-							self.get_html(category_url + "/default/" + str(page)).text,
+					category_urls = []
+					# Category 167 does not have a real page
+					if category == "167":
+						for dict_td_list in BeautifulSoup(
+							self.get_html(
+								"https://pinyin.sogou.com/dict/cate/index/180"
+							).text,
 							"html.parser",
-						).find_all("div", class_="dict_detail_block"):
-							key_download = dict.find("div", class_="detail_title").a
-							download_urls[
-								# For dictionaries like 天线行业/BSA
-								key_download.string.replace("/", "-")
-								.replace(",", "-")
-								.replace("|", "-")
-								.replace("\\", "-")
-								.replace("'", "-")
-								if key_download.string
-								# For dictionaries without a name like index 15946
-								else BeautifulSoup(
-									self.get_html(
-										"https://pinyin.sogou.com"
-										+ key_download["href"]
-									).text,
-									"html.parser",
-								)
-								.find("div", class_="dict_info_str")
-								.string
-							] = dict.find("div", class_="dict_dl_btn").a["href"]
-						cnt += self.download(download_urls, category_path)
-		print(f"Total download: {cnt} dictionaries.")
+						).find_all("div", class_="citylistcate"):
+							category_urls.append(
+								"https://pinyin.sogou.com" + dict_td_list.a["href"]
+							)
+					else:
+						category_urls.append(
+							"https://pinyin.sogou.com/dict/cate/index/" + category
+						)
+					for category_url in category_urls:
+						pages = (
+							BeautifulSoup(
+								self.get_html(category_url).text, "html.parser"
+							)
+							.find("div", id="dict_page_list")
+							.find_all("a")
+						)
+						page_n = 2 if len(pages) < 2 else int(pages[-2].string) + 1
+						result_futures += [
+							executor.submit(
+								self.get_download_links,
+								category_url,
+								page,
+								category_path,
+							)
+							for page in range(1, page_n)
+						]
+			for future in concurrent.futures.as_completed(result_futures):
+				try:
+					download_urls, category_path = future.result()
+					for name, url in download_urls.items():
+						executor.submit(self.download, name, url, category_path)
+				except Exception as e:
+					print(e)
 
 
 def check_category_index(value):
@@ -144,12 +159,13 @@ if __name__ == "__main__":
 		"--directory",
 		"-d",
 		default="sougou_dict",
+		type=Path,
 		help="The directory to save dictionaries, which are divided by categories in DIR.\n"
 		"The default directory is sougou_dict.",
 		metavar="DIR",
 	)
 	parser.add_argument(
-		"--category",
+		"--categories",
 		"-c",
 		nargs="+",
 		type=check_category_index,
@@ -167,4 +183,8 @@ if __name__ == "__main__":
 	)
 	args = parser.parse_args()
 	SGSpider = SougouSpider()
-	SGSpider.download_dicts(args.directory, args.category, args.skip_category)
+	SGSpider.download_dicts(
+		args.directory,
+		None if args.categories is None else list(set(args.categories)),
+		args.skip_category,
+	)
